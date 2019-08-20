9aaacd9d43c46b8eeed1da514383f9444ef3be72
Lucien Lee <lucien@thundertoken.tw>
2019-08-20 16:15:31 +0800

    fix version deps

--- contracts/Badges/Badges.sol
+++ contracts/Badges/Badges.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.25;
 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Enumerable.sol";
--- src/contracts/contracts.js
+++ src/contracts/contracts.js
@@ -1,1 +1,1 @@
-module.exports = []
+module.exports = ["Links","LinksNFT","ERC20Vendable","VendingMachine","Burner","BurnerVendor","Badges"]
